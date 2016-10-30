f014c43233ac60b067d69ba5054429ba16e997d3
login2030 <login2030@gmail.com>
2016-10-30 21:10:28 +0500

    сделал верхнюю менюшку + навигация по документу с плавным скроллом и хешем

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,34 @@
 /* global app, device */
 window.addEventListener('load', e=> {
+	/* Высота первого экрана */
+	// app.firstSection.adaptive();
+	// app.w.resize(e=> {
+	// 	app.firstSection.adaptive();
+	// });
+	/* --- */
 	/* Слайдер */
 	if (device.desktop() || device.tablet()) {
 		app.sliderCompany.init();
 	}
+	/* --- */
+	
+	/* Top menu */
+	app.topMenu.toggle();
+	app.w.scroll(e => {
+		app.topMenu.toggle();
+	});
+	app.navigation.make();
+	/* --- */
+	
+	/* Клики на сайте */
+	app.w.click(e=> {
+		let $el = $(e.target);
+		// Навигация в меню
+		if ($el.hasClass('js-nav-a')) {
+			e.preventDefault();
+			app.navigation.scrollTo($el);
+		}
+	});
+	/* --- */
+	
 });
--- src/js/functions.js
+++ src/js/functions.js
@@ -1,8 +1,12 @@
 /* global app */
-app.sliderCompany = (function() {
+app.w = $(window);
+app.hb = $('html, body');
+
+app.sliderCompany = (()=> {
 	let el = $('#js-slider-company'),
 		count = parseInt(el.attr('data-count')),
 		itemStr = '';
+
 	return {
 		init() {
 			for (let i = count - 1; i > 0; i--) {
@@ -19,6 +23,68 @@ app.sliderCompany = (function() {
 				pauseOnFocus: false,
 				autoplaySpeed: 2000
 			});
+		}
+	};
+})();
+
+// app.firstSection = (()=> {
+// 	let el = $('#first-section'),
+// 		wh = app.w.height();
+
+// 	return {
+// 		adaptive() {
+// 			el.height(wh);
+// 		}
+// 	};
+// })();
+
+app.topMenu = (()=> {
+	let el = $('#js-nav-top');
+	
+	return {
+		items: el.find('.c-nav-top__a'),
+		el: el,
+		visible() {
+			el.removeClass('c-nav-top_hide');
+		},
+		hide() {
+			el.addClass('c-nav-top_hide');
+		},
+		toggle() {
+			if (app.w.scrollTop() <= 0) {
+				this.visible();
+			} else {
+				this.hide();
+			}
+		}
+	};
+})();
+
+app.navigation = (()=> {
+	let el = $('.js-menu-point');
+	
+	return {
+		make() {
+			el.waypoint(function(d) {
+				let section, link;
+				section = $(this.element);
+				if (d === "up") {
+					section = section.prev();
+				}
+
+				link = app.topMenu.el.find('div a[href="#' + section.attr("id") + '"]');
+				app.topMenu.items.removeClass("c-nav-top__a_is-active");
+				link.addClass("c-nav-top__a_is-active");
+			}, {
+				offset: '20%'
+			});
+		},
+		scrollTo($el) {
+			let href = $el.attr('href');
+			app.hb.stop().animate({
+				scrollTop: $(href).offset().top - 60
+			}, 1200);
+			window.location.hash = href;
 		}
 	};
 })();
