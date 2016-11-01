6a7c53ad386bded9083d59b2fda852ca547162ce
login2030 <login2030@gmail.com>
2016-11-01 23:24:47 +0500

    Доделал блок с табами

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,9 @@
 /* global app, device */
 window.addEventListener('load', e=> {
+	let tabsHeader = $('#js-tabs-header'),
+		tabsContent = $('#js-tabs-content');
+	
+	
 	/* Высота первого экрана */
 	// app.firstSection.adaptive();
 	// app.w.resize(e=> {
@@ -27,6 +31,14 @@ window.addEventListener('load', e=> {
 		if ($el.hasClass('js-nav-a')) {
 			e.preventDefault();
 			app.navigation.scrollTo($el);
+		}
+		
+		// Табы
+		if ($el.hasClass('c-learn-tabs__header-item')) {
+			tabsHeader.find('.c-learn-tabs__header-item_is-active').removeClass('c-learn-tabs__header-item_is-active');
+			$el.addClass('c-learn-tabs__header-item_is-active');
+			tabsContent.find('.c-learn-tabs__content-item_is-active').removeClass('c-learn-tabs__content-item_is-active');
+			tabsContent.find('.c-learn-tabs__content-item').eq(parseInt($el.attr('data-tab'))).addClass('c-learn-tabs__content-item_is-active');
 		}
 	});
 	/* --- */
